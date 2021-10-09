c7d706b15ab8c290eddfec05d2028458afb98d50
Ewasuu <edwardcolmenarez89@gmail.com>
2021-10-09 12:07:57 -0400

    tokens removed

--- helpers/api_data.js
+++ helpers/api_data.js
@@ -1,6 +1,6 @@
 const API = {
     URL: 'https://api.mapbox.com/geocoding/v5/mapbox.places/',
-    TOKEN: '.json?access_token=pk.eyJ1IjoiZXdhc3UiLCJhIjoiY2t1aXBzZnNkMHVhMTJub2ZicTFkeG9jdSJ9.0V5jvqovgddv2gsk_GRrBw',
+    TOKEN: '.json?access_token=',
     LIMIT: '&limit=5',
     LANG: '&language=es'
 }
--- helpers/api_w.js
+++ helpers/api_w.js
@@ -2,7 +2,7 @@ const APIW = {
     URL: 'https://api.openweathermap.org/data/2.5/weather?',
     LAT_: 'lat=',
     LON_: '&lon=',
-    TOKEN: '&appid=3aee8aa96a1ae3783780663928d15aeb&units=metric&lang=es'
+    TOKEN: '&appid=&units=metric&lang=es'
 }
 
 module.exports = APIW
